670a54d2823e5f50686d7ebb87caf828e326cec0
sophiahusak <72futbolplayer@gmail.com>
2018-07-12 18:50:56 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,4 +1,4 @@
-###### Code Your Solutions Here!!!
+def first_recycle_bin{"address"=>"E 227 St/Bronx River Pkway","site_type"=>"Subproperty","longitude"=>"-73.864223918","borough"=>"Bronx","latitude"=>"40.890848989","park_site_name"=>"227th St. Plgd"}###### Code Your Solutions Here!!!
 
 #Your code should work with a hash that looks like the one below (in comments).
 #The specs (which call your tests) are using these exact hashes 
@@ -6,12 +6,7 @@
 #they're dynamically brought in from the NYC Open Data API
 
 # first_recycle_bin =  {
-#   "address"=>"E 227 St/Bronx River Pkway",
-#   "site_type"=>"Subproperty",
-#   "longitude"=>"-73.864223918",
-#   "borough"=>"Bronx",
-#   "latitude"=>"40.890848989",
-#   "park_site_name"=>"227th St. Plgd"
+#   
 # }
 
 
