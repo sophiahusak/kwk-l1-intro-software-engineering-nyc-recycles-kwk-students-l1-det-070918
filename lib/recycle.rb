c4f5a0fa75d1768a479b8213131ae20c2aaaff7c
sophiahusak <72futbolplayer@gmail.com>
2018-07-12 18:57:19 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -12,10 +12,16 @@ def first_recycle_bin{"address"=>"E 227 St/Bronx River Pkway","site_type"=>"Subp
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
+def address(recycle)
+  recycle.each_key
+  puts recycle
+end 
+end
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
-
+def borough(recycle)
+  recycle.each_value
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
